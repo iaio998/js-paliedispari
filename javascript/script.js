83a650d51494e786b38a4ba587c9e5ffc0e776a4
iaio998 <depalatisiacopo@gmail.com>
2023-10-17 15:40:16 +0200

    Added verification ''palindromo''

--- javascript/script.js
+++ javascript/script.js
@@ -10,4 +10,11 @@ btn.addEventListener("click", function () {
   for (let i = 0; i < userString.length; i++) {
     firstArray.push(userString[i]);
   }
+  console.log(firstArray);
+  console.log(firstArray.join(""), firstArray.reverse().join(""));
+  msg =
+    firstArray.join("") === firstArray.reverse().join("")
+      ? "E' palindromo"
+      : "Non e' palindromo";
+  console.log(msg);
 });
