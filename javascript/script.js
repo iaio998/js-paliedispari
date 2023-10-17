146efc1f9a2535246060f4f230eac845bc4d7beb
iaio998 <depalatisiacopo@gmail.com>
2023-10-17 16:04:03 +0200

    Added working exercise with for reversed

--- javascript/script.js
+++ javascript/script.js
@@ -3,18 +3,26 @@ const inputEl = document.getElementById("data");
 const resultEl = document.querySelector(".alert");
 let msg;
 
+// ESERCIZIO PALINDROMO
+// Decommentando il commentato si ha la soluzione senza usare il for inverso
+
 btn.addEventListener("click", function () {
-  const firstArray = [];
+  //   const firstArray = [];
   let userString = inputEl.value;
+  let newString = "";
   console.log(userString);
-  for (let i = 0; i < userString.length; i++) {
-    firstArray.push(userString[i]);
+  for (let i = userString.length - 1; i >= 0; i--) {
+    console.log(userString[i]);
+    newString += userString[i];
   }
-  console.log(firstArray);
-  console.log(firstArray.join(""), firstArray.reverse().join(""));
-  msg =
-    firstArray.join("") === firstArray.reverse().join("")
-      ? "E' palindromo"
-      : "Non e' palindromo";
+  console.log(newString);
+  msg = userString === newString ? "E' palindromo" : "Non e' palindromo";
   console.log(msg);
+  //   console.log(firstArray);
+  //   console.log(firstArray.join(""), firstArray.reverse().join(""));
+  //   msg =
+  //     firstArray.join("") === firstArray.reverse().join("")
+  //       ? "E' palindromo"
+  //       : "Non e' palindromo";
+  //   console.log(msg);
 });
