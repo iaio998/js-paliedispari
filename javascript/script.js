f675c1ac1a059ffded511a547f773f7e2342691c
iaio998 <depalatisiacopo@gmail.com>
2023-10-17 15:39:35 +0200

    Added for cicle

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,12 @@ const btn = document.querySelector("button");
 const inputEl = document.getElementById("data");
 const resultEl = document.querySelector(".alert");
 let msg;
+
+btn.addEventListener("click", function () {
+  const firstArray = [];
+  let userString = inputEl.value;
+  console.log(userString);
+  for (let i = 0; i < userString.length; i++) {
+    firstArray.push(userString[i]);
+  }
+});
