d05c10fbb2b79838dc99bc1b7489237236128186
iaio998 <depalatisiacopo@gmail.com>
2023-10-17 16:30:49 +0200

    Fixed structure two exercise

--- javascript/script.js
+++ javascript/script.js
@@ -8,16 +8,16 @@ let msg;
 
 btn.addEventListener("click", function () {
   // //    const firstArray = [];
-  //   let userString = inputEl.value;
-  //   let newString = "";
-  //   console.log(userString);
-  //   for (let i = userString.length - 1; i >= 0; i--) {
-  //     console.log(userString[i]);
-  //     newString += userString[i];
-  //   }
-  //   console.log(newString);
-  //   msg = userString === newString ? "E' palindromo" : "Non e' palindromo";
-  //   console.log(msg);
+  let userString = inputEl.value;
+  let newString = "";
+  console.log(userString);
+  for (let i = userString.length - 1; i >= 0; i--) {
+    console.log(userString[i]);
+    newString += userString[i];
+  }
+  console.log(newString);
+  msg = userString === newString ? "E' palindromo" : "Non e' palindromo";
+  console.log(msg);
   // //   console.log(firstArray);
   // //   console.log(firstArray.join(""), firstArray.reverse().join(""));
   // //   msg =
@@ -30,3 +30,8 @@ btn.addEventListener("click", function () {
 // ESERCIZIO PARI E DISPARI
 
 const inputEl2 = document.getElementById("pari", "dispari");
+
+inputEl2.addEventListener("click", function () {
+  inputEl.classList.remove("d-none");
+  let userSelection = inputEl2.value;
+});
